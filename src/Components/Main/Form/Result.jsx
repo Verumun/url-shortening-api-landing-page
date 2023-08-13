c1da9733113860a46a586d01dc48f7f505b9f373
Verumun <nevezekiel3@gmail.com>
2023-08-13 21:43:31 +0100

    Copy-clipboard functionality done

--- src/Components/Main/Form/Result.jsx
+++ src/Components/Main/Form/Result.jsx
@@ -1,10 +1,23 @@
 import "./Result.scss";
-import React from 'react';
+import React from "react";
 import { useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 const Result = () => {
+  const [shortenLink, setShortenLink] = useState("https://rel.ink./4lyk");
+  const [copied, setCopied] = useState(false)
 
-    const [shortenLink, setShortenLink] = useState("https://rel.ink./4lyk")
+  const handleCopyClick = () => {
+    // Perform the copy operation here if needed
+
+    // Update the state to show "Copied!" text
+    setCopied(true);
+
+    // Reset the "Copied!" text after a certain time (e.g., 2 seconds)
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
 
   return (
     <div className="result-wrapper">
@@ -13,8 +26,18 @@ const Result = () => {
       </div>
       <hr />
       <div className="flex">
-        <div id="shortened-link" className="shortened-link">{shortenLink}</div>
-        <button className="">Copy</button>
+        <div id="shortened-link" className="shortened-link">
+          {shortenLink}
+        </div>
+
+        <CopyToClipboard 
+          text={shortenLink}
+          onCopy={() => setCopied(true)}
+        >
+          <button className={copied ? "copied" : ""} onClick={handleCopyClick}>
+          {copied ? "Copied!" : "Copy"}
+          </button>
+        </CopyToClipboard>
       </div>
     </div>
   );
