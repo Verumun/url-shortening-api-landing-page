903257de0408bc448d753f0fe279f600d4671b1d
Verumun <nevezekiel3@gmail.com>
2023-08-16 00:18:56 +0100

    shortening functionality 97% done

--- src/Components/Main/Form/Form.jsx
+++ src/Components/Main/Form/Form.jsx
@@ -1,25 +1,147 @@
-import React from 'react'
-import './Form.scss'
-// import backgroundImage from '../../../images/bg-boost-desktop.svg'
-import Result from './Result'
+// import React, { useState } from 'react'
+// import './Form.scss'
+// // import backgroundImage from '../../../images/bg-boost-desktop.svg'
+// import Result from './Result'
 
-const Form =()=> {
+// const Form =()=> {
+//   const [inputValue, setInputValue] = useState("")
+//   return (
+//     <div>
+//         <div className="form-wrapper">
+//             <form action="">
+//                 <div className="input-control">
+//                     <input setInputValue ={setInputValue} type="text" placeholder='Shorten a link here..' />
+//                     <div className="error"></div>
+//                 </div>
+//                 <button className='shorten'>Shorten it!</button>
+//             </form>
+            
+//         </div>
+
+//         <Result inputValue ={inputValue}/>
+//     </div>
+//   )
+// }
+
+// export default Form;
+
+// import React, { useState } from 'react';
+// import './Form.scss';
+// import Result from './Result';
+
+// const Form = () => {
+//   const [inputValue, setInputValue] = useState("");
+//   const [shortenLink, setShortenLink] = useState("");
+//   const [error, setError] = useState("");
+
+//   const handleFormSubmit = async (e) => {
+//     e.preventDefault();
+
+//     // Basic URL validation
+//     const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
+//     if (!urlRegex.test(inputValue)) {
+//       setError("Invalid URL");
+//       return;
+//     }
+
+//     try {
+//       const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${inputValue}`);
+//       const data = await response.json();
+//       if (data.ok) {
+//         setShortenLink(data.result.short_link);
+//         setError("");
+//       } else {
+//         console.error("Error shortening link");
+//         setError("Error shortening link");
+//       }
+//     } catch (error) {
+//       console.error("Error:", error);
+//       setError("Error occurred");
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <div className="form-wrapper">
+//         <form onSubmit={handleFormSubmit}>
+//           <div className="input-control">
+//             <input
+//               type="text"
+//               placeholder='Shorten a link here..'
+//               value={inputValue}
+//               onChange={(e) => setInputValue(e.target.value)}
+//             />
+//             <div className="error">{error}</div>
+//           </div>
+//           <button type="submit" className='shorten'>Shorten it!</button>
+//         </form>
+//       </div>
+
+//       <Result inputValue={inputValue} shortenLink={shortenLink} />
+//     </div>
+//   );
+// }
+
+// export default Form;
+
+import React, { useState } from 'react';
+import './Form.scss';
+import Result from './Result';
+
+const Form = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [originalInputValue, setOriginalInputValue] = useState("");
+  const [shortenLink, setShortenLink] = useState("");
+  const [error, setError] = useState("");
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    // Basic URL validation
+    const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
+    if (!urlRegex.test(inputValue)) {
+      setError("Invalid URL");
+      return;
+    }
+
+    try {
+      const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${inputValue}`);
+      const data = await response.json();
+      if (data.ok) {
+        setShortenLink(data.result.short_link);
+        setOriginalInputValue(inputValue); // Store original input value
+        setInputValue(""); // Clear the input field
+        setError("");
+      } else {
+        console.error("Error shortening link");
+        setError("Error shortening link");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setError("Error occurred");
+    }
+  };
+
   return (
     <div>
-        <div className="form-wrapper">
-            <form action="">
-                <div className="input-control">
-                    <input type="text" placeholder='Shorten a link here..' />
-                    <div className="error"></div>
-                </div>
-                <button className='shorten'>Shorten it!</button>
-            </form>
-            
-        </div>
+      <div className="form-wrapper">
+        <form onSubmit={handleFormSubmit}>
+          <div className="input-control">
+            <input
+              type="text"
+              placeholder='Shorten a link here..'
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <div className="error">{error}</div>
+          </div>
+          <button type="submit" className='shorten'>Shorten it!</button>
+        </form>
+      </div>
 
-        <Result/>
+      {shortenLink && <Result inputValue={originalInputValue} shortenLink={shortenLink} />}
     </div>
-  )
+  );
 }
 
 export default Form;
--- src/Components/Main/Form/Result.jsx
+++ src/Components/Main/Form/Result.jsx
@@ -1,28 +1,69 @@
+// import "./Result.scss";
+// import React from "react";
+// import { useState } from "react";
+// import CopyToClipboard from "react-copy-to-clipboard";
+
+// const Result = ({inputValue}) => {
+//   console.log(inputValue)
+//   const [shortenLink, setShortenLink] = useState("https://rel.ink./4lyk");
+//   const [copied, setCopied] = useState(false)
+
+//   const handleCopyClick = () => {
+//     // Perform the copy operation here if needed
+
+//     // Update the state to show "Copied!" text
+//     setCopied(true);
+
+//     // Reset the "Copied!" text after a certain time (e.g., 2 seconds)
+//     setTimeout(() => {
+//       setCopied(false);
+//     }, 2000);
+//   };
+
+//   return (
+//     <div className="result-wrapper">
+//       <div id="inputed-link" className="inputed-link">
+//         https://www.frontendmentor.io
+//       </div>
+//       <hr />
+//       <div className="flex">
+//         <div id="shortened-link" className="shortened-link">
+//           {shortenLink}
+//         </div>
+
+//         <CopyToClipboard 
+//           text={shortenLink}
+//           onCopy={() => setCopied(true)}
+//         >
+//           <button className={copied ? "copied" : ""} onClick={handleCopyClick}>
+//           {copied ? "Copied!" : "Copy"}
+//           </button>
+//         </CopyToClipboard>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Result;
+
 import "./Result.scss";
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
 
-const Result = () => {
-  const [shortenLink, setShortenLink] = useState("https://rel.ink./4lyk");
-  const [copied, setCopied] = useState(false)
+const Result = ({ inputValue, shortenLink }) => {
+  const [copied, setCopied] = useState(false);
 
   const handleCopyClick = () => {
-    // Perform the copy operation here if needed
-
-    // Update the state to show "Copied!" text
     setCopied(true);
-
-    // Reset the "Copied!" text after a certain time (e.g., 2 seconds)
     setTimeout(() => {
       setCopied(false);
-    }, 3000);
+    }, 2000);
   };
 
   return (
     <div className="result-wrapper">
       <div id="inputed-link" className="inputed-link">
-        https://www.frontendmentor.io
+        {inputValue}
       </div>
       <hr />
       <div className="flex">
@@ -30,12 +71,12 @@ const Result = () => {
           {shortenLink}
         </div>
 
-        <CopyToClipboard 
+        <CopyToClipboard
           text={shortenLink}
-          onCopy={() => setCopied(true)}
+          onCopy={handleCopyClick}
         >
-          <button className={copied ? "copied" : ""} onClick={handleCopyClick}>
-          {copied ? "Copied!" : "Copy"}
+          <button className={copied ? "copied" : ""}>
+            {copied ? "Copied!" : "Copy"}
           </button>
         </CopyToClipboard>
       </div>
@@ -44,3 +85,4 @@ const Result = () => {
 };
 
 export default Result;
+
